26dd8071f98f08b3c14035c7bbefb7ba516b2c75
minnukota381 <minnukota381@gmail.com>
2024-10-01 07:31:15 +0530

    updated controller

--- src/main/java/com/main/controller/StudentController.java
+++ src/main/java/com/main/controller/StudentController.java
@@ -20,7 +20,7 @@ public class StudentController {
     public List<Students> getAllStudents() {
         return repo.findAll();
     }
-
+    
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/students/{id}")
     public Students getStudent(@PathVariable int id) {
